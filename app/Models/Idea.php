228a5ecfe239af67bfc47eefbabd3d10935eb69a
OforiAlexander <alexanderofori07@gmail.com>
2023-07-26 00:42:27 +0000

    status created and tests still fails

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -37,4 +37,10 @@ class Idea extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
 }
--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -35,8 +35,8 @@
                     </div>
                     <div class="flex items-center space-x-2 mt-4 md:mt-0">
                         <div
-                            class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4 hover:bg-gray-300 transition duration-150 ease-in">
-                            Open</div>
+                            class="{{ $idea->status->classes }} text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4 hover:bg-gray-300 transition duration-150 ease-in">
+                            {{ $idea->status->name }}</div>
                         <button
                         x-data="{ isOpen : false }"
                         @click="isOpen = !isOpen"
